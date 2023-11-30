a002a7562187eb66506c99d764ee1fdb3ef39bb9
dprcold <nike111000@yandex.ru>
2023-11-30 17:26:26 +0300

    upd-checkboxes

--- src/components/FilterCheckboxGroup/FilterCheckboxGroup.tsx
+++ src/components/FilterCheckboxGroup/FilterCheckboxGroup.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import { useDispatch } from 'react-redux';
 
 import customCheckBox from '../assets/Checkbox.png';
@@ -16,6 +16,7 @@ enum CheckboxIds {
   ThreeTransfers = 5,
 }
 
+
 export const FilterCheckboxGroup: React.FC = () => {
   const uiState = useTypeSelector((state) => state.ui);
   const dispatch = useDispatch();
@@ -26,30 +27,37 @@ export const FilterCheckboxGroup: React.FC = () => {
     { id: CheckboxIds.TwoTransfers, title: '2 пересадка', state: uiState.checkboxTwoTransfers },
     { id: CheckboxIds.ThreeTransfers, title: '3 пересадка', state: uiState.checkboxThreeTransfers },
   ];
-
   const changeCheckbox = (checkboxId: number) => {
-    if (checkboxId === CheckboxIds.All && !uiState.checkboxAll) {
-      dispatch({ type: ActionTypes.SET_ALL_CHECKBOX_TRUE });
-    } else {
-      dispatch({ type: ActionTypes.SET_ALL_CHECKBOX_FALSE });
-    }
-
-    if (checkboxId === CheckboxIds.NoTransfers) {
-      dispatch({ type: ActionTypes.SET_CHECKBBOX_NO_TRANSFERS });
-    }
-
-    if (checkboxId === CheckboxIds.OneTransfer) {
-      dispatch({ type: ActionTypes.SET_CHECKBOX_ONE_TRANSFER });
-    }
-
-    if (checkboxId === CheckboxIds.TwoTransfers) {
-      dispatch({ type: ActionTypes.SET_CHECKBOX_TWO_TRANSFERS });
-    }
-    
-    if (checkboxId === CheckboxIds.ThreeTransfers) {
-      dispatch({ type: ActionTypes.SET_CHECKBOX_THREE_TRANSFERS });
-    } 
+   if(checkboxId === CheckboxIds.All){
+    dispatch({ type: ActionTypes.SET_CHECKBOX_NAME_ALL });
+   }
+   if(checkboxId === CheckboxIds.NoTransfers){
+    dispatch({ type : ActionTypes.SET_CHECKBOX_NAME_NO_TRANSFERS });
+   }
+   if(checkboxId === CheckboxIds.OneTransfer){
+    dispatch({ type : ActionTypes.SET_CHECKBOX_NAME_ONE_TRANSFER });
+   }
+   if(checkboxId === CheckboxIds.TwoTransfers){
+    dispatch({ type : ActionTypes.SET_CHECKBOX_NAME_TWO_TRANSFERS });
+   }
+   if(checkboxId === CheckboxIds.ThreeTransfers){
+    dispatch({ type : ActionTypes.SET_CHECKBOX_NAME_THREE_TRANFERS });
+   }
+   if(checkboxId === CheckboxIds.All && uiState.checkboxAll) {
+    dispatch({type: ActionTypes.SET_CHECKBOX_NAME_ALL_REVERSE})
+   }
   };
+  useEffect(() => {
+    const activeCheckboxes = checkboxes.slice(1).every(item => item.state);
+    
+    if (activeCheckboxes && !uiState.checkboxAll) {
+      dispatch({ type: ActionTypes.SET_CHECKBOX_NAME_ALL_TO_TRUE });
+    } else if (!activeCheckboxes && uiState.checkboxAll) {
+      dispatch({ type: ActionTypes.SET_CHECKBOX_NAME_ALL_TO_FALSE });
+    }
+  }, [uiState, dispatch]);
+  
+  
   return (
     <div className={style.container}>
       <div className={style.title}>количество пересадок</div>
--- src/redux/uiReducer.ts
+++ src/redux/uiReducer.ts
@@ -1,3 +1,4 @@
+import { TRUE } from 'sass';
 import { ActionTypes } from './actions';
 
 interface uiState {
@@ -18,44 +19,54 @@ const initialState = {
 
 export const uiReducer = (state: uiState = initialState, action: any) => {
   switch (action.type) {
-    case ActionTypes.SET_ALL_CHECKBOX_TRUE:
+   case ActionTypes.SET_CHECKBOX_NAME_ALL:
+    return {
+      ...state,
+      checkboxAll: true,
+      checkboxNoTransfers: true,
+      checkboxOneTransfers: true,
+      checkboxTwoTransfers: true,
+      checkboxThreeTransfers: true,
+    }
+   case ActionTypes.SET_CHECKBOX_NAME_NO_TRANSFERS: 
+    return {
+      ...state,
+      checkboxNoTransfers: !state.checkboxNoTransfers,
+    }
+    case ActionTypes.SET_CHECKBOX_NAME_ONE_TRANSFER: 
+    return {
+      ...state,
+      checkboxOneTransfers: !state.checkboxOneTransfers,
+    }
+    case ActionTypes.SET_CHECKBOX_NAME_TWO_TRANSFERS: 
+    return {
+      ...state,
+      checkboxTwoTransfers: !state.checkboxTwoTransfers,
+    }
+    case ActionTypes.SET_CHECKBOX_NAME_THREE_TRANFERS: 
+    return {
+      ...state,
+      checkboxThreeTransfers: !state.checkboxThreeTransfers,
+    }
+    case ActionTypes.SET_CHECKBOX_NAME_ALL_TO_TRUE:
       return {
         ...state,
         checkboxAll: true,
-        checkboxNoTransfers: true,
-        checkboxOneTransfers: true,
-        checkboxTwoTransfers: true,
-        checkboxThreeTransfers: true,
-      };
-    case ActionTypes.SET_ALL_CHECKBOX_FALSE:
-      return {
-        ...state,
-        checkboxAll: false,
-      };
-    case ActionTypes.SET_CHECKBBOX_NO_TRANSFERS:
-      return {
-        ...state,
-        checkboxNoTransfers: !state.checkboxNoTransfers,
-        checkboxAll: false,
-      };
-    case ActionTypes.SET_CHECKBOX_ONE_TRANSFER:
-      return {
-        ...state,
-        checkboxOneTransfers: !state.checkboxOneTransfers,
-        checkboxAll: false,
-      };
-    case ActionTypes.SET_CHECKBOX_TWO_TRANSFERS:
-      return {
-        ...state,
-        checkboxTwoTransfers: !state.checkboxTwoTransfers,
-        checkboxAll: false,
-      };
-    case ActionTypes.SET_CHECKBOX_THREE_TRANSFERS:
-      return {
-        ...state,
-        checkboxThreeTransfers: !state.checkboxThreeTransfers,
-        checkboxAll: false,
-      };
+      }
+      case ActionTypes.SET_CHECKBOX_NAME_ALL_TO_FALSE:
+        return {
+          ...state, 
+          checkboxAll: false,
+        }
+        case ActionTypes.SET_CHECKBOX_NAME_ALL_REVERSE:
+          return {
+            ...state,
+            checkboxAll: false,
+            checkboxNoTransfers: false,
+            checkboxOneTransfers: false,
+            checkboxTwoTransfers: false,
+            checkboxThreeTransfers: false,
+          }
     default:
       return state;
   }
