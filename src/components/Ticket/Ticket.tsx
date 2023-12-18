9e8d098d49182c284f6f92234ec38c9cb9cd28f4
dprcold <nike111000@yandex.ru>
2023-12-18 16:32:26 +0300

    fix

--- src/components/Ticket/Ticket.tsx
+++ src/components/Ticket/Ticket.tsx
@@ -24,7 +24,7 @@ const formatStopsWord = (stopsCount: number): string => {
 const isNextDayDate = (dateLeft: Date, dateRight: Date): string | undefined => {
   const daysDifference = dateRight.getDate() - dateLeft.getDate();
   if (daysDifference) {
-    return ` +${daysDifference}`;
+    return ' +1';
   }
 };
 
